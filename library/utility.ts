8351904b16d0313c1e5f8df80e85233fd590705b
Stuart Williams <spookdejur@hotmail.com>
2025-05-10 15:16:07 -0700

    Demo,4

--- library/utility.ts
+++ library/utility.ts
@@ -1,15 +1,12 @@
 "use strict";
 
-import os from 'os';
-import path from 'node:path';
+import os from "os";
+import path from "node:path";
 
-export function tempFile(name:string='temp.txt')
-{
-if(!name)
-{
-    throw new Error(
-        'name is required'
-    );
-}
-
+export function tempFile(name: string = "temp.txt") {
+  if (!name) {
+    throw new Error("name is required");
+  }
+  const tmpdir = os.tmpdir();
+  return path.join(tmpdir, name);
 }
